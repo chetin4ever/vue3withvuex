bb3b5e08bfa2ecb4d83fff8ec65b2825dfa6e2f8
chetin4ever <hetan009@gmail.com>
2021-02-02 20:27:51 +0530

    creating app with vue using store and its method like mutations,actions,getters

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,46 @@
-import { createStore } from 'vuex'
-
+import { createStore } from "vuex"
+import axios from "axios"
 export default createStore({
   state: {
+    counter: 0,
+    colorCode: "blue",
   },
   mutations: {
+    incrementCounter(state, randomNumber) {
+      state.counter += randomNumber
+    },
+    decrementCounter(state, randomNumber) {
+      state.counter -= randomNumber
+    },
+    setColorCode(state, newValue) {
+      state.colorCode = newValue
+    },
   },
   actions: {
+    incrementCounter({ commit }) {
+      axios(
+        "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new"
+      ).then((response) => {
+        console.log("response", response.data)
+        commit("incrementCounter", response.data)
+      })
+    },
+    decrementCounter({ commit }) {
+      axios(
+        "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new"
+      ).then((response) => {
+        console.log("response", response.data)
+        commit("decrementCounter", response.data)
+      })
+    },
+    setColorCode({ commit }, newValue) {
+      commit("setColorCode", newValue)
+    },
   },
-  modules: {
-  }
+  modules: {},
+  getters: {
+    counterSuare(state) {
+      return state.counter * state.counter
+    },
+  },
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="counter" :style="{ color: $store.state.colorCode }">
+    {{ $store.state.counter }}
+  </div>
+  <div class="counter-square">
+    {{ $store.state.counter }}<sup>2</sup> = {{ $store.getters.counterSuare }}
+  </div>
+  <div class="button">
+    <button @click="$store.dispatch('incrementCounter')">+</button>
+    <button @click="$store.dispatch('decrementCounter')">-</button>
+  </div>
+  <div>
+    <input v-model="colorCode" type="text" placeholder="Enter color code" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  computed: {
+    colorCode: {
+      get() {
+        return this.$store.state.colorCode;
+      },
+      set(newValue) {
+        this.$store.dispatch("setColorCode", newValue);
+        console.log(newValue);
+      },
+    },
+  },
+};
 </script>
+<style >
+.counter {
+  font-size: 80px;
+}
+.button button {
+  margin: 0 10px;
+  width: 100px;
+  font-size: 50px;
+}
+div {
+  margin: 10px;
+}
+</style>
